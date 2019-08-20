9e5eb38c43bb2747ec5227e744d4e862f2d42cfb
Alvaro Mourino <alvaro@mourino.net>
2019-08-20 16:07:50 -0500

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='dnstls',
+    version='0.0.1',
+    description="N26's DNS/TLS Proxy",
+    author='Alvaro Mourino',
+    author_email='alvaro@mourino.net',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Internet :: Proxy Servers',
+    ],
+)
